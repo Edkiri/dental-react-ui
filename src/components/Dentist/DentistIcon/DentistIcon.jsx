9a748087cf4a98afc249c90d567d46157961db0d
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-06-27 20:23:49 +0200

    feat: default dentist profile pic

--- src/components/Dentist/DentistIcon/DentistIcon.jsx
+++ src/components/Dentist/DentistIcon/DentistIcon.jsx
@@ -2,9 +2,10 @@ import './DentistIcon.css';
 import HectorImg from '../../../../assets/hector.jpeg';
 import MatiImg from '../../../../assets/mati.jpeg';
 import DaniImg from '../../../../assets/dani.jpeg';
+import DefaultImg from '../../../../assets/default-profile-pic.webp';
 
 export function DentistIcon({ dentistId }) {
-  let imgUrl;
+  let imgUrl = DefaultImg;
   if (dentistId === '649abe9787fdb58569abee1e') imgUrl = HectorImg;
   if (dentistId === '649abe9787fdb58569abee24') imgUrl = MatiImg;
   if (dentistId === '649abe9787fdb58569abee21') imgUrl = DaniImg;
