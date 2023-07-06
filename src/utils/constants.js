ae453ca5e7a6e46d1263736ea886af6100e70bb9
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-07-06 22:15:15 +0200

    try: deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:5000/api/v1';
+export const API_URL = 'https://dental-api-production.up.railway.app/api/v1';
 
 export const APPOINTMENT_STATUS = {
   REQUESTED: 'requested',
